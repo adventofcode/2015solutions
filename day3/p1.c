6bb28a041feb54fd57f196c2efefc6476e1a8eaf
Toyam Cox <Vaelatern@gmail.com>
2015-12-03 01:22:23 -0500

    Add day 3 code

---
+++ day3/p1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct house {
+	long x;
+	long y;
+	struct house *next;
+};
+char AddHouse(long, long, struct house *);
+
+int main(void)
+{
+	struct house *current;
+	current = (struct house *) malloc(sizeof(struct house));
+	*current = (struct house) {0,0,NULL};
+	char c;
+	long x = 0, y = 0;
+	unsigned long houses = 1; // The initial house.
+	while ((c = fgetc(stdin)) != EOF) {
+		switch (c) {
+		case '^':
+			y++;
+			break;
+		case 'v':
+			y--;
+			break;
+		case '>':
+			x++;
+			break;
+		case '<':
+			x--;
+			break;
+		default:
+			printf("Unknown character input: %c.\n", c);
+			break;
+		}
+		houses += AddHouse(x,y,current);
+	}
+	printf("Houses visited: %lu\n", houses);
+	return 0;
+}
+
+char AddHouse(long x, long y, struct house *haus)
+{
+	while (haus->x != x || haus->y != y) {
+		if (haus->next) {
+			haus = haus->next;
+		} else {
+			haus->next = (struct house *)
+				malloc(sizeof(struct house));
+			*(haus->next) = (struct house) {x,y,NULL};
+			return 1;
+		}
+	}
+	return 0;
+}
---
+++ day3/p2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct house {
+	long x;
+	long y;
+	struct house *next;
+};
+char AddHouse(long, long, struct house *);
+
+int main(void)
+{
+	struct house *current;
+	current = (struct house *) malloc(sizeof(struct house));
+	*current = (struct house) {0,0,NULL};
+	char c;
+	long x[2], y[2];
+	x[0] = y[0] = x[1] = y[1] = 0;
+	char roboOrReal = 0;
+	unsigned long houses = 1; // The initial house.
+	while ((c = fgetc(stdin)) != EOF) {
+		switch (c) {
+		case '^':
+			y[roboOrReal]++;
+			break;
+		case 'v':
+			y[roboOrReal]--;
+			break;
+		case '>':
+			x[roboOrReal]++;
+			break;
+		case '<':
+			x[roboOrReal]--;
+			break;
+		default:
+			printf("Unknown character input: %c.\n", c);
+			break;
+		}
+		houses += AddHouse(x[roboOrReal],y[roboOrReal],current);
+		roboOrReal = !roboOrReal;
+	}
+	printf("Houses visited: %lu\n", houses);
+	return 0;
+}
+
+char AddHouse(long x, long y, struct house *haus)
+{
+	while (haus->x != x || haus->y != y) {
+		if (haus->next) {
+			haus = haus->next;
+		} else {
+			haus->next = (struct house *)
+				malloc(sizeof(struct house));
+			*(haus->next) = (struct house) {x,y,NULL};
+			return 1;
+		}
+	}
+	return 0;
+}
