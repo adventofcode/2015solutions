8fe3af22da0b97d278d3dbf2dde3513ba316e187
JustANull <reid.levenick@gmail.com>
2015-12-12 12:57:16 -0600

    Refactored day12/p2a.rs

--- day12/p2a.rs
+++ day12/p2a.rs
@@ -15,29 +15,7 @@ use serde_json::{Value, from_reader};
 
 use std::io;
 
-fn remove_red(val: &mut Value) {
-    match val {
-        &mut Value::Array(ref mut arr) => {
-            for mut item in arr.iter_mut() {
-                remove_red(&mut item);
-            }
-        },
-        &mut Value::Object(ref mut obj) => {
-            if obj.iter()
-                .filter(|&(_, v)| v.as_string().map(|s| s == "red").unwrap_or(false))
-                .next().is_some() {
-                    obj.clear();
-            } else {
-                for (_, mut item) in obj.iter_mut() {
-                    remove_red(&mut item);
-                }
-            }
-        },
-        _ => {},
-    }
-}
-
-fn sum_of_value(val: &Value) -> i64 {
+fn sum_value(val: &Value) -> i64 {
     match val {
         &Value::I64(i) => i,
         &Value::U64(u) => u as i64,
@@ -46,7 +24,7 @@ fn sum_of_value(val: &Value) -> i64 {
             let mut sum = 0;
 
             for item in arr.iter() {
-                sum += sum_of_value(item);
+                sum += sum_value(&item);
             }
 
             sum
@@ -55,7 +33,11 @@ fn sum_of_value(val: &Value) -> i64 {
             let mut sum = 0;
 
             for (_, item) in obj.iter() {
-                sum += sum_of_value(item);
+                if let Some(true) = item.as_string().map(|s| s == "red") {
+                    return 0;
+                }
+
+                sum += sum_value(&item);
             }
 
             sum
@@ -68,11 +50,10 @@ fn main() {
     let stdin = io::stdin();
     let stdin = stdin.lock();
 
-    let mut input: Value = from_reader(stdin)
+    let input: Value = from_reader(stdin)
         .expect("the input should be properly formatted");
 
-    remove_red(&mut input);
-    let sum = sum_of_value(&input);
+    let sum = sum_value(&input);
 
     println!("Sum: {}", sum);
 }
