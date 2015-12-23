c38872e9ff38fca7c9c3406b2d4757c910922215
JustANull <reid.levenick@gmail.com>
2015-12-23 00:05:30 -0600

    Added Rust solutions for day 15

---
+++ day15/p2a.rs
@@ -0,0 +1,84 @@
+use std::cmp::max;
+use std::collections::HashMap;
+use std::io::{self, BufRead};
+
+trait DropLast {
+    fn drop_last(&self) -> &Self;
+}
+
+impl DropLast for str {
+    fn drop_last(&self) -> &str {
+        &self[0..self.len() - 1]
+    }
+}
+
+fn parse_line(line: &str) -> (String, i64, i64, i64, i64, i64) {
+    let mut segments = line.split_whitespace();
+
+    let name = segments.nth(0).unwrap().drop_last().to_owned();
+    let capacity = segments.nth(1)
+        .expect("the input should be properly formatted")
+        .drop_last().parse()
+        .expect("the input should be properly formatted");
+    let durability = segments.nth(1)
+        .expect("the input should be properly formatted")
+        .drop_last().parse()
+        .expect("the input should be properly formatted");
+    let flavor = segments.nth(1)
+        .expect("the input should be properly formatted")
+        .drop_last().parse()
+        .expect("the input should be properly formatted");
+    let texture = segments.nth(1)
+        .expect("the input should be properly formatted")
+        .drop_last().parse()
+        .expect("the input should be properly formatted");
+    let calories = segments.nth(1)
+        .expect("the input should be properly formatted")
+        .parse()
+        .expect("the input should be properly formatted");
+
+    (name, capacity, durability, flavor, texture, calories)
+}
+
+fn get_max_value(remaining: i64, num_seen: usize, stats: &Vec<(i64, i64, i64, i64, i64)>, cur_capacity: i64, cur_durability: i64, cur_flavor: i64, cur_texture: i64, cur_calories: i64) -> Option<i64> {
+    if num_seen >= stats.len() {
+        if cur_calories == 500 {
+            Some(max(0, cur_capacity) * max(0, cur_durability) * max(0, cur_flavor) * max(0, cur_texture))
+        } else {
+            None
+        }
+    } else {
+        stats.iter().skip(num_seen).map(|&(capacity, durability, flavor, texture, calories)| {
+            let mut max_value = None;
+            for amt in 0..remaining + 1 {
+                if let Some(value) = get_max_value(remaining - amt, num_seen + 1, stats,
+                                                   cur_capacity + amt * capacity,
+                                                   cur_durability + amt * durability,
+                                                   cur_flavor + amt * flavor,
+                                                   cur_texture + amt * texture,
+                                                   cur_calories + amt * calories) {
+                    if !max_value.is_some() || value > max_value.unwrap() {
+                        max_value = Some(value);
+                    }
+                }
+            }
+
+            max_value
+        }).max().unwrap()
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+
+    let mut ingredient_stats = HashMap::new();
+    for line in stdin.lines() {
+        let (name, capacity, durability, flavor, texture, calories) =
+            parse_line(&line.expect("reading from stdin should succeed"));
+        ingredient_stats.insert(name, (capacity, durability, flavor, texture, calories));
+    }
+
+    let max_value = get_max_value(100, 0, &ingredient_stats.values().map(|t| *t).collect(), 0, 0, 0, 0, 0);
+    println!("Max: {:?}", max_value);
+}
